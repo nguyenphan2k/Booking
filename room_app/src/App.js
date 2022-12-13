02bec29beb5afee645f49980887bcc1ab114a6f1
phan nguyen <phannguyen7565@gmail.com>
2022-12-13 10:57:47 +0700

    Second Commit

--- room_app/src/App.js
+++ room_app/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassWord from './pages/ForgotPassWord';
+import Offers from './pages/Offers';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <h1 className='text-2xl'>Hello</h1>   
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />}/>
+          <Route path='/signin' element={<SignIn />}/>
+          <Route path='/signup' element={<SignUp />}/>
+          <Route path='/forgot-password' element={<ForgotPassWord />}/>
+          <Route path='/offers' element={<Offers />}/>
+        </Routes>
+      </Router>
+      <Footer />
     </>
   );
 }
